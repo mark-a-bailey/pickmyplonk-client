35a96bdca81d42a16f612e54038b14dd838df59b
mark-a-bailey <beefybailey@gmail.com>
2017-12-09 20:13:54 +0000

    wrapping all links and routes in a router to fix compile and build errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
@@ -8,23 +8,25 @@ import RouteNavItem from "./components/RouteNavItem";
 class App extends Component {
     render() {
         return (
-            <div className="App container">
-                <Navbar fluid collapseOnSelect>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <Link to="/">Home</Link>
-                        </Navbar.Brand>
-                        <Navbar.Toggle />
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav pullRight>
-                            <RouteNavItem href="/signup">Signup</RouteNavItem>
-                            <RouteNavItem href="/login">Login</RouteNavItem>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-                <Routes />
-            </div>
+            <Router>
+                <div className="App container">
+                    <Navbar fluid collapseOnSelect>
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                                <Link to="/">Home</Link>
+                            </Navbar.Brand>
+                            <Navbar.Toggle />
+                        </Navbar.Header>
+                        <Navbar.Collapse>
+                            <Nav pullRight>
+                                <RouteNavItem href="/signup">Signup</RouteNavItem>
+                                <RouteNavItem href="/login">Login</RouteNavItem>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                    <Routes />
+                </div>
+            </Router>
         );
     }
 }
