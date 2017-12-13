b0005c953c9e8aee66d1770697eed2f26b318052
mark-a-bailey <beefybailey@gmail.com>
2017-12-13 14:15:58 +0000

    demo circleci build to paul

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,7 +7,7 @@ export default class Home extends Component {
             <div className="Home">
                 <div className="lander">
                     <h1>Pick My Plonk</h1>
-                    <p>A simple wine tracking app</p>
+                    <p>A simple wine tracking app v2</p>
                 </div>
             </div>
         );
