0bab484df406a01bcdf8acf1dbc45910f9acfdbb
mark-a-bailey <beefybailey@gmail.com>
2017-12-17 18:40:18 +0000

    adding login page and calls and config to cognito (does not store state yet)

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import Http404NotFound from "./containers/errors/Http404NotFound";
 
 export default () =>
     <Switch>
         <Route path="/" exact component={Home} />
+
+        <Route path="/login" exact component={Login} />
 
         { /* Finally, catch all unmatched routes */ }
         <Route component={Http404NotFound} />
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+    cognito: {
+        USER_POOL_ID: "eu-west-2_IOEbDlJq6",
+        APP_CLIENT_ID: "3drq7ih6pt89l467asph0ts2bt"
+    }
+};
