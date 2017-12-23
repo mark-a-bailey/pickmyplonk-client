08ef82cb6e8037b87cb66228af13cc97dac06ec7
mark-a-bailey <beefybailey@gmail.com>
2017-12-23 19:28:44 +0000

    making app tests work for both shallow copy and also memory router

---
+++ src/App.shallow.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+    let app;
+
+    beforeEach(() => {
+        app = shallow(<App />);
+    });
+
+    it('renders without crashing', () => {
+        shallow(<App />);
+    });
+
+    /*it('App renders nested components', () => {
+        expect(app.find('Add').length).toEqual(1);
+        expect(app.find('List').length).toEqual(1);
+    });
+
+    it('onAdd updates List', () => {
+        const add = app.find('Add').first();
+        add.props().onAdd('Name 1');
+        app.update();
+
+        const list = app.find('List').first();
+        const listData = list.props().data;
+        expect(listData.length).toEqual(1);
+        expect(listData[0]).toEqual('Name 1');
+    });*/
+
+});
