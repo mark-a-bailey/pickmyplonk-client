3ece2bd1e6b17aa2faddb63a1d4f6936b9efbe33
mark-a-bailey <beefybailey@gmail.com>
2017-12-23 20:16:07 +0000

    add signup flow and email confirmation code

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import AppliedRoute from "./components/AppliedRoute";
 import Http404NotFound from "./containers/errors/Http404NotFound";
 
@@ -9,6 +10,7 @@ export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps} />
         <AppliedRoute path="/login" exact component={Login} props={childProps} />
+        <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
 
         { /* Finally, catch all unmatched routes */ }
         <Route component={Http404NotFound} />
